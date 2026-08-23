d2d5d084188dd3bb78b46abc96f4a65dde95a09a
Abdul Basith <159468770+basthep@users.noreply.github.com>
2026-08-24 00:12:25 +0530

    Configure CORS with dynamic client URL

--- server/server.js
+++ server/server.js
@@ -17,7 +17,12 @@ const app = express();
 // MIDDLEWARE
 // ============================================
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
